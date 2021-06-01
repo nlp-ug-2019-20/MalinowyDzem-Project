166366dbaa0a6bfc5f237bce4e95bb3662a7d43a
Kasia Hałas <Kasiahalas97@Gmail.com>
2021-06-01 11:36:06 +0200

    remove options

--- chatbot.js
+++ chatbot.js
@@ -101,8 +101,6 @@ function getRecipeIgredients(recipeId){
        options.remove()
       })
     }
-
-
       function getRecipeInstruction(recipeId){
         var secondOutput = document.createElement("div");
         let url="https://api.spoonacular.com/recipes/"+recipeId+"/analyzedInstructions"+pass
